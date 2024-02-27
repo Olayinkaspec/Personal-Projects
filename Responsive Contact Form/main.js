e38be58cc967e8cec4fc8af54bd105db5ec26a12
Olayinkaspec <oyinkansolahod@gmail.com>
2024-02-27 15:33:43 +0100

    Updated

--- Responsive Contact Form/main.js
+++ Responsive Contact Form/main.js
@@ -10,32 +10,35 @@ function verify() {
 
     let error = [];
 
-    if (name.length < 5) {
-        error.push("Please enter a valid Name");
+    if (name == []) {
+        error += "Please enter a valid Name.\n";
     }
 
-    if (subject.length < 8) {
-        error.push("Please enter a valid Subject");
+    if (subject == []) {
+        error += "Please enter a valid Subject.\n";
     }
 
     if (isNaN(phone) || phone.length != 11) {
         error.push("Please enter a valid Phone Number");
     }
 
-    if (email.IndexOf("@") == -1 || email.length < 6) {
-        error.push("Please enter a valid Email");
+    if (email == []) {
+        error += "Please enter a valid Email.\n";
+    } else if (!validateEmail(email)) {
+        error += "Please enter a valid Email.\n";
     }
 
-    if (message.length <= 50) {
-        error.push("Please enter more than 50 Characters");
+    if (message == []) {
+        error += "Please enter Message.\n";
     }
 
-    if (error.length > 0) {
-        errorInfo.innerHTML = error.join("<br>");
+    if (error != []) {
+        alert(error);
         return false;
-    } else {
-        alert(
-            "Submitted");
-        return true;
     }
+}
+
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
